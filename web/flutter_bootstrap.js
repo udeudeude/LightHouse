aa8719cba2f41dbf6b8887326b03f22eab3c7780
udeudeude <udeudeude@gmail.com>
2026-09-16 08:53:48 -0400

    fix: bump web asset version for renamed path

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '3';
+const lighthouseBuildVersion = '4';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
