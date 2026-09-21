a47de6d25dbc65ba1fcdb12e5ccf4d5d6a66d2a1
udeudeude <udeudeude@gmail.com>
2026-09-21 19:13:20 -0400

    chore: bump web bundle for UI refinements

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '14';
+const lighthouseBuildVersion = '15';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
