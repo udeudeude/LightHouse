2bd6aa71685ea290bbc58fc082c0919e41ed97ef
udeudeude <udeudeude@gmail.com>
2026-09-21 10:56:52 -0400

    chore: bump web bundle for Zendo mode

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '10';
+const lighthouseBuildVersion = '11';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
