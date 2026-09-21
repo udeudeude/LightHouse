a42e0e24ad0d5f549f054300d595859165f8d097
udeudeude <udeudeude@gmail.com>
2026-09-21 14:59:59 -0400

    fix: move web Remote relay to current secure WebSocket broker

--- lib/platform/remote_mqtt_web.dart
+++ lib/platform/remote_mqtt_web.dart
@@ -3,10 +3,10 @@ import 'package:mqtt_client/mqtt_client.dart';
 
 MqttClient createRemoteMqttClient(String clientId) {
   final client = MqttBrowserClient(
-    'wss://broker.hivemq.com:8884/mqtt',
+    'wss://broker.emqx.io:8084/mqtt',
     clientId,
   );
-  client.port = 8884;
+  client.port = 8084;
   client.websocketProtocols = MqttClientConstants.protocolsSingleDefault;
   return client;
 }
