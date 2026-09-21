d4940482f981ab876a85b34b9b4267cd51faf1c9
udeudeude <udeudeude@gmail.com>
2026-09-21 12:24:01 -0400

    extend light footprints for Zendo 2.0 shapes

--- lib/domain/light_element.dart
+++ lib/domain/light_element.dart
@@ -2,7 +2,11 @@ import 'physical_point.dart';
 
 enum PyramidSize { small, medium, large }
 
-enum PyramidPose { upright, flat }
+enum PyramidPose { upright, flat, wedgeTriangle, wedgeRectangle, blockFlat }
+
+enum LightPieceKind { pyramid, wedge, block }
+
+enum ZendoPieceSetMode { classic, zendo20, both }
 
 enum IlluminationPattern { full, wall }
 
@@ -14,6 +18,7 @@ class LightElement {
     required this.position,
     required this.headingDegrees,
     required this.illumination,
+    this.kind = LightPieceKind.pyramid,
   });
 
   final String id;
@@ -22,6 +27,7 @@ class LightElement {
   final PhysicalPoint position;
   final double headingDegrees;
   final IlluminationPattern illumination;
+  final LightPieceKind kind;
 
   LightElement copyWith({
     PyramidSize? size,
@@ -29,6 +35,7 @@ class LightElement {
     PhysicalPoint? position,
     double? headingDegrees,
     IlluminationPattern? illumination,
+    LightPieceKind? kind,
   }) {
     return LightElement(
       id: id,
@@ -37,6 +44,7 @@ class LightElement {
       position: position ?? this.position,
       headingDegrees: headingDegrees ?? this.headingDegrees,
       illumination: illumination ?? this.illumination,
+      kind: kind ?? this.kind,
     );
   }
 
@@ -47,6 +55,7 @@ class LightElement {
     'position': position.toJson(),
     'headingDegrees': headingDegrees,
     'illumination': illumination.name,
+    'kind': kind.name,
   };
 
   factory LightElement.fromJson(Map<String, Object?> json) => LightElement(
@@ -60,6 +69,7 @@ class LightElement {
     illumination: IlluminationPattern.values.byName(
       json['illumination']! as String,
     ),
+    kind: LightPieceKind.values.byName((json['kind'] as String?) ?? 'pyramid'),
   );
 
   @override
@@ -70,9 +80,10 @@ class LightElement {
       other.pose == pose &&
       other.position == position &&
       other.headingDegrees == headingDegrees &&
-      other.illumination == illumination;
+      other.illumination == illumination &&
+      other.kind == kind;
 
   @override
   int get hashCode =>
-      Object.hash(id, size, pose, position, headingDegrees, illumination);
+      Object.hash(id, size, pose, position, headingDegrees, illumination, kind);
 }
