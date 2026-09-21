79ca76d6aac334bfca34e4597a0942f10ecb119a
udeudeude <udeudeude@gmail.com>
2026-09-21 12:44:31 -0400

    chore: bump web bundle for Zendo footprint refinement

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '11';
+const lighthouseBuildVersion = '12';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
