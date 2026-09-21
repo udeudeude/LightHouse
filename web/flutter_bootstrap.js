fadb97ef9b026ef7b18ca9d5e1de6d871dd31ed4
udeudeude <udeudeude@gmail.com>
2026-09-21 08:40:59 -0400

    chore: bump web bundle for Dice Bubble polish

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '9';
+const lighthouseBuildVersion = '10';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
