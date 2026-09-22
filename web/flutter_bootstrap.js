26ff33d7c1b3469a26d077da1fbe8f79598e58f5
udeudeude <udeudeude@gmail.com>
2026-09-22 09:41:31 -0400

    chore: bump web build for toy order fix

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '16';
+const lighthouseBuildVersion = '17';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
