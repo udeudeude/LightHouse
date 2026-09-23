1c2d62df160bb79185a3d5cfca8c22b238cb6115
udeudeude <udeudeude@gmail.com>
2026-09-22 20:14:21 -0400

    chore: bump web build for icons dice and localization

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '18';
+const lighthouseBuildVersion = '19';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
