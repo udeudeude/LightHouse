b31245f4064045398d8c6949ed8b2ed0c3243d40
udeudeude <udeudeude@gmail.com>
2026-09-23 15:10:11 -0400

    style: format language arrow regression test

--- test/application/app_language_test.dart
+++ test/application/app_language_test.dart
@@ -8,9 +8,13 @@ void main() {
     AppLanguageController.notifier.value = AppLanguage.english;
   });
 
-
   test('language picker arrow explicitly requests text presentation', () {
-    expect(languagePickerGlyph.runes.toList(), <int>[0x41, 0x2194, 0xFE0E, 0x3042]);
+    expect(languagePickerGlyph.runes.toList(), <int>[
+      0x41,
+      0x2194,
+      0xFE0E,
+      0x3042,
+    ]);
     expect(languagePickerGlyph.codeUnits.contains(0xFE0F), isFalse);
   });
 
