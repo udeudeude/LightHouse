e65ea39c059b5d98a2c5d8dd01757d012debf82b
udeudeude <udeudeude@gmail.com>
2026-09-23 17:51:01 -0400

    chore: refresh web runtime cache version

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '29';
+const lighthouseBuildVersion = '30';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
