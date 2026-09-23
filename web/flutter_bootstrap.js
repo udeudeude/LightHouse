24ecffb56af14cf8b175239c983320be41eff322
udeudeude <udeudeude@gmail.com>
2026-09-23 08:42:14 -0400

    chore: bump web build for real polyhedral dice

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '21';
+const lighthouseBuildVersion = '22';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
