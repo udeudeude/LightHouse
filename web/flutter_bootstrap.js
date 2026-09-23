69d180dd4d2a425184817a6c1f977207f2dcb45e
udeudeude <udeudeude@gmail.com>
2026-09-23 13:16:24 -0400

    chore: bump web cache for iOS install fix

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '26';
+const lighthouseBuildVersion = '27';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
