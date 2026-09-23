0bf51f2f5d7cbf361e5d0d67d3e1b30ee31621ed
udeudeude <udeudeude@gmail.com>
2026-09-22 22:06:35 -0400

    chore: bump web build for toy grid and languages

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '20';
+const lighthouseBuildVersion = '21';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
