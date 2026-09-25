8d510bc4e4879cd9ac7239bd5682e995cfd5c89d
udeudeude <udeudeude@gmail.com>
2026-09-25 13:39:09 -0400

    chore: bump web/flutter_bootstrap.js to web build 35

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,7 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const lighthouseBuildVersion = '34';
+const lighthouseBuildVersion = '35';
 
 for (const build of (_flutter.buildConfig?.builds ?? [])) {
   if (build.mainJsPath) {
